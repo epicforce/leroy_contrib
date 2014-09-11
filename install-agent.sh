dde0e0a4c766d8c590164642f86d35c2b442ba5a
leroyjenkinsplugin <j@epicforce.net>
2014-09-11 12:38:14 -0400

    Added installscript

---
+++ install-agent.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PWD=`pwd`
+crontab -l | grep -v check-agent-running > /tmp/o
+echo "*/3 * * * *  ${PWD}/check-agent-running.sh > /dev/null 2>&1" >> /tmp/o
+crontab /tmp/o
+rm -rf /tmp/o
+echo "== Agent installed in crontab running as: ${USER} =="
+crontab -l
