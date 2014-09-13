74b1df5d9a94b1b402ad46b1b1b403a8b70109e3
leroyjenkinsplugin <j@epicforce.net>
2014-09-13 09:29:44 -0400

    woof

--- install-agent.sh
+++ install-agent.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 PWD=`pwd`
 crontab -l | grep -v check-agent-running > /tmp/o
-echo "*/3 * * * *  cd ${PWD}; ${PWD}/check-agent-running.sh > /dev/null 2>&1" >> /tmp/o
+if [ ${?} -ne 0 ]; then
+ echo "*/3 * * * *  cd ${PWD}; ${PWD}/check-agent-running.sh > /dev/null 2>&1" > /tmp/o
+else
+ echo "*/3 * * * *  cd ${PWD}; ${PWD}/check-agent-running.sh >> /dev/null 2>&1" >> /tmp/o
+fi
+
 crontab /tmp/o
 rm -rf /tmp/o
 echo "== Agent installed in crontab running as: ${USER} =="
